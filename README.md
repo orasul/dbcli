f73ee11d8d0302dcf42ed0aa5e73a39b615b8708
Rasul Osmanov <orasul@mail.ru>
2020-06-28 11:11:57 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # dbcli
 DataBase cli utilities
+* mcli - Mongo client based on pymongo
+* rcli - Redis client based on pymongo
+
+#### mcli usage examples
+``` bash
+mcli list-dbs
+mcli list-cols -d books
+mcli list-docs -d books -c authors
+mcli show-doc -d aws -c images -i ami-eede2314
+mcli show-doc --database aws --collection images -i ami-eede2314
+mcli show-doc --database aws --collection images --document-id ami-eede2314
+mcli show-doc -d gcloud -c elements -o 5ef869cf316dd267c64be59c
+mcli show-doc -d gcloud -c elements --document-object-id 5ef869cf316dd267c64be59c
+```
+
+Inspired by [dbcli](https://www.dbcli.com).
+
+## Limitations
+
+Should avoid using in production and for huge amount of data.
+
+
+## License & Authors
+
+- Author:: Rasul Osmanov
+
+```text
+Copyright (c) 2020, Rasul Osmanov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ mcli.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import click
+import pymongo
+import json
+import datetime
+from bson.objectid import ObjectId
+
+def json_handler(x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    elif isinstance(x, ObjectId):
+        return str(x)
+    else:
+        raise TypeError(x)
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option('-h', '--host', default='localhost', help='MongoDB host.')
+@click.option('-p', '--port', default='27017', help='MongoDB port.')
+def list_dbs(host, port):
+    """List databases in Mongo"""
+    cl = pymongo.MongoClient()
+    dbs = cl.list_databases()
+    for db in dbs:
+        print(db["name"])
+
+@cli.command()
+@click.option('-h', '--host', default='localhost', help='MongoDB host.')
+@click.option('-p', '--port', default='27017', help='MongoDB port.')
+@click.option('-d', '--database', default='devops', help='Database name.')
+def list_cols(host, port, database):
+    """List collections in DB"""
+    cl = pymongo.MongoClient()
+    db = cl[database]
+    cols = db.list_collections()
+    for col in cols:
+        print(col["name"])
+
+@cli.command()
+@click.option('-h', '--host', default='localhost', help='MongoDB host.')
+@click.option('-p', '--port', default='27017', help='MongoDB port.')
+@click.option('-d', '--database', default='devops', help='Database name.')
+@click.option('-c', '--collection', default='projects', help='Collection.')
+@click.option('-f', '--filter', 'filt', default='{}', help='Filter json.')
+def list_docs(host, port, database, collection, filt):
+    """List document ids from collection"""
+    cl = pymongo.MongoClient()
+    db = cl[database]
+    projects = db[collection]
+    filt = json.loads(filt)
+    data = projects.find(filt)
+    for doc in data:
+        if isinstance(doc["_id"], ObjectId): print("ObjectId: ", end="")
+        print(doc["_id"])
+
+@cli.command()
+@click.option('-h', '--host', default='localhost', help='MongoDB host.')
+@click.option('-p', '--port', default='27017', help='MongoDB port.')
+@click.option('-d', '--database', default='devops', help='Database name.')
+@click.option('-c', '--collection', default='projects', help='Collection.')
+@click.option('-i', '--document-id', help='Document _id value.')
+@click.option('-o', '--document-object-id', help='Document ObjectId value.')
+def show_doc(host, port, database, collection, document_id, document_object_id):
+    """List document ids from collection"""
+    cl = pymongo.MongoClient()
+    db = cl[database]
+    collection = db[collection]
+    if document_id:
+        filt = '{'+f'"_id":"{document_id}"'+'}'
+        filt = json.loads(filt)
+    elif document_object_id:
+        filt = {'_id': ObjectId(document_object_id)}
+    else:
+        raise RuntimeError("Either --document-object-id or --document-id should be set")
+    data = collection.find_one(filt)
+    print(json.dumps(data, indent=4, default=json_handler))
+
+@cli.command()
+@click.option('-h', '--host', default='localhost', help='MongoDB host.')
+@click.option('-p', '--port', default='27017', help='MongoDB port.')
+@click.option('-d', '--database', default='devops', help='Database name.')
+@click.option('-c', '--collection', default='projects', help='Collection.')
+def add_doc(host, port, database, collection):
+    """List document ids from collection"""
+    cl = pymongo.MongoClient()
+    db = cl[database]
+    projects = db[collection]
+    edit_json = {'title': 'titlename', 'key1': 'value1', 'key2': 'value2'}
+    edited_document = click.edit(json.dumps(edit_json, indent=4), require_save=True, extension='.json')
+    if edited_document:
+        try:
+            data = json.loads(edited_document)
+            print(json.dumps(data, indent=4, default=json_handler))
+            projects.insert(data)
+        except Exception as ex:
+            print('Json?', ex)
+
+if __name__ == '__main__':
+    cli()
