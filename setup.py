b37c3287b49a4c5dbaf761438c958615d6b0e842
orasul <orasul@mail.ru>
2021-10-12 00:28:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dbcli_mongo_redis",
-    version="0.3.4",
+    version="0.3.5",
     description="Mongo and redis cli",
     long_description=long_description,
     long_description_content_type="text/markdown",
