3d5bf3f52c20a946e7b3a7d8d97ef85fee04791d
Rasul <orasul@mail.ru>
2022-01-09 17:47:34 +0300

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dbcli_mongo_redis",
-    version="0.3.6",
+    version="0.3.7",
     description="Mongo and redis cli",
     long_description=long_description,
     long_description_content_type="text/markdown",
